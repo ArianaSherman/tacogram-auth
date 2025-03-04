d82c932deaebae9d03b3b5c149f82da36e2b7d05
Ariana Sherman <ariana.sherman@kellogg.northwestern.edu>
2025-03-04 16:55:02 +0000

    added initial security, hide password in view, encrypt password, authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   get("/login", {:controller => "sessions", :action => "new"})
   get("/logout", {:controller => "sessions", :action => "destroy"})
-
+  get("",{:controller=>"posts",:action=>"index"}) #landing page, no need to specify file path, goes directly to posts 
+  
   # TODO: map landing page (aka root route) to controller and action
   # get("/", {:controller => "", :action => ""})
 end
